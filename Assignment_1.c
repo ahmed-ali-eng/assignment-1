2d8411f4a3df9a9b25bdbb00cb8932604e465bde
maJOrr1337 <81903080+maJOrr1337@users.noreply.github.com>
2021-04-14 16:57:19 +0200

    Struct Student

---
+++ Assignment_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+struct student
+{
+    char name[50];
+    int ID;
+    int day,month,year;
+    int score;
+    struct student* next;
+};
+int main()
+{
+    printf("Welcome");
+    
+
+
+
+
+    return 0;
+}
